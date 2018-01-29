e9e7e7cbbb77a8e38c8b4b9c688e58461ecdaa5d
truenagual <true.nagual@gmail.com>
2018-01-29 23:41:30 +0100

    Partially ready random articles on the front page

--- inc/post-list.php
+++ inc/post-list.php
@@ -1,15 +1,10 @@
 <a href="<?php the_permalink(); ?>">
     <article id="post-<?php the_ID(); ?>"  <?php post_class('entry-box'); ?> >
         <?php the_post_thumbnail('full', array('class' => 'entry-img'));?>
-        <div class="entry-mask"></div>
-        <div class="entry-text-layer">
-            <div class="entry-padding" >
-                <div class="entry-content">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                    <div class="entry-text">
-                        <?php  the_content('');?>
-                    </div>
-                </div>
+        <div class="entry-content">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <div class="entry-text">
+                <?php  the_content('');?>
             </div>
         </div>
     </article>
